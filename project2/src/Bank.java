dcb39c9a1290b820e20b794daaecf3022d05b7a0
ayushijain1198 <49468933+ayushijain1198@users.noreply.github.com>
2021-05-04 14:27:32 +0530

    Add files via upload

---
+++ project2/src/Bank.java
@@ -0,0 +1,35 @@
+public class Bank {
+    int bankId;
+    String bankName;
+    String bankCity;
+    int noOfCustomers;
+
+    public Bank(){
+        this(100);
+
+    }
+    public Bank(int bankId){
+        this(bankId,"SBI");
+
+    }
+    public Bank(int bankId, String bankName){
+        this(bankId, bankName,"mumbai");
+    }
+    public Bank(int bankId, String bankName, String bankCity){
+        this(bankId, bankName , bankCity, 10000);
+    }
+
+
+    public Bank(int bankId, String bankName, String bankCity, int noOfCustomers){
+        this.bankId=bankId;
+        this.bankName=bankName;
+        this.bankCity=bankCity;
+        this.noOfCustomers=noOfCustomers;
+    }
+
+    public String getBankDetails(){
+        return "ID: "+bankId+"Name: "+bankName+"city: "+bankCity+"No of customers = "+noOfCustomers;
+
+    }
+}
+
---
+++ project2/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String args){
+        Bank bank = null;
+        bank = new Bank();
+        System.out.println(bank.getBankDetails());
+    }
+}
+
